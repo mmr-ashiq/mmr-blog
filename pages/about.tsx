cc75e2cc4ea2a67fd5696c69b9c6056d6caf62ed
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-08-09 20:26:21 +0600

    final version deploy 1.0

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+
+type Props = {}
+
+const about = (props: Props) => {
+  return (
+    <div>
+        This blog site create by <a href="https://www.linkedin.com/in/mmr-ashiq/">Mizanur Rahman Ashiq</a>
+    </div>
+  )
+}
+
+export default about
